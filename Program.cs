9ae92913c98b4119ecbff4893a920b93eacb7d07
Paul Law <paul@80nd.co.uk>
2025-01-27 08:42:36 +0000

    Episode 3 - BBP Pi Formula

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 ﻿using System.Numerics;
 
+// Program to sense check approach for calculating PI on my 8-bit relay computer using
+// the Bailey-Borwein-Plouffe formula (which calculates the first n digits of Pi in base 16).
+// This program will also provide values which can confirm the relay computer's output.
+
+// The challenge is to calculate PI to 20 decimal places but actually I'll be doing it to 22 places. 
+// To accuarately represent PI to 20 decimal places requires at least 17 hex digits but as my computer
+// works with 8-bit values I need to use 18 hex digits (9 bytes).
+
 // Lets start with the known value for PI to 20 decimal places + 1 extra to be accurate within 17 hex digits.
 const decimal PI = 3.141592653589793238462M;
+
+#region Console Colour Helpers
+const string NORMAL = "\x1b[39m";
+const string GREEN = "\x1b[92m";
+const string YELLOW = "\x1b[93m";
+#endregion
 
 
 
@@ -70,6 +84,63 @@ static decimal ConvertFixedPointBinaryToDecimal(FixedPointBinary value, int prec
     // Can now parse string as decimal result
     return decimal.Parse(result);
 };
+
+
+
+
+// Part C - Use Bailey-Borwein-Plouffe formula to calculate PI in base 16 (to 17 hex digit precision)
+Console.WriteLine("\n\nC) Calculate PI using Bailey-Borwein-Plouffe formula:\n");
+var calcPiBin = CalculatePi((17 + 2) * 4);      // Calculate PI with extra 2 hex digits of precision
+var roundedCalcPiBin = calcPiBin with           // to allow truncating result to 17 hex digits
+{
+    FractionalPart = calcPiBin.FractionalPart >> 8,
+    Precision = calcPiBin.Precision - 8
+};
+
+Console.WriteLine("\nOutcome:");
+Console.WriteLine($"Calculated PI Value: {calcPiBin}");
+Console.WriteLine($"Truncated to 17bp:   {roundedCalcPiBin}");
+Console.WriteLine($"Known PI Value:      {piBin}\n");
+
+var roundedCalcPiDec = ConvertFixedPointBinaryToDecimal(roundedCalcPiBin, 20);
+Console.WriteLine("\nOutcome:");
+Console.WriteLine($"PI Original Value:   {roundedCalcPiDec} ({roundedCalcPiDec.Scale} dp)");
+Console.WriteLine($"Calculated PI Value: {piDec} ({piDec.Scale} dp)");
+
+// Function to calculate PI in base 16 using the Bailey-Borwein-Plouffe formula
+// Calculation is performed to given fractional part precision (in bits).
+static FixedPointBinary CalculatePi(int precision)
+{
+    var size = precision + 4;       // Full size of values allow extra word for integral part
+    var sum = new BigInteger(0);    // Variable to store sum
+
+    for (int k = 0; k < 17; k++)
+    {
+        var dv = 8 * k;
+        var a = (new BigInteger(4) << (precision)) / (dv + 1);
+        var b = (new BigInteger(2) << (precision)) / (dv + 4);
+        var c = (new BigInteger(1) << (precision)) / (dv + 5);
+        var d = (new BigInteger(1) << (precision)) / (dv + 6);
+        var v = a - b - c - d;
+        var vs = v >> (4 * k);
+
+        Console.WriteLine($"{YELLOW}k = {k:d2}{NORMAL}");
+        Console.WriteLine($"    {a.ToString($"b{size}")[..size]} (4/{dv + 1:d3})");
+        Console.WriteLine($"  - {b.ToString($"b{size}")[..size]} (2/{dv + 4:d3})");
+        Console.WriteLine($"  - {c.ToString($"b{size}")[..size]} (1/{dv + 5:d3})");
+        Console.WriteLine($"  - {d.ToString($"b{size}")[..size]} (1/{dv + 6:d3})");
+        Console.WriteLine($"{GREEN}  = {v.ToString($"b{size}")[..size]} {v.ToString($"x{size/4}")[..(size/4)]}{NORMAL}");
+        Console.WriteLine($" >> {vs.ToString($"b{size}")[..size]} {vs.ToString($"x{size/4}")[..(size/4)]}");
+        Console.WriteLine($"  + {sum.ToString($"b{size}")[..size]} {sum.ToString($"x{size/4}")[..(size/4)]}");
+        sum += vs;
+        Console.WriteLine($"{GREEN}  = {sum.ToString($"b{size}")[..size]} {sum.ToString($"x{size/4}")[..(size/4)]}\n{NORMAL}");
+    }
+
+    // Create bit masks required to isolate integral and fractional parts when multiplying
+    var integralMask = new BigInteger(0xF) << precision;
+    var fractionalMask = (new BigInteger(0x1) << precision) - 1;
+    return new FixedPointBinary((byte)((sum & integralMask) >> precision), sum & fractionalMask, precision);
+}
 
 
 
